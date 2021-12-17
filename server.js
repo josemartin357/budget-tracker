04ce43cedb274376c9cb36182bf9f329ba0c36a2
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-12-17 00:52:06 -0600

    adjusted mongoose.connect in server.js to link db to mongo atlas

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetdb", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
